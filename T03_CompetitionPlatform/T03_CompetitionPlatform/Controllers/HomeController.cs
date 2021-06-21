2f5559f316a9c867b0a63a82d5b97576b6da16d3
Muhd Dandarawie Bin Rohani <s10208266@connect.np.edu.sg>
2021-06-21 19:34:21 +0800

    A bit of homepage attempted

--- T03_CompetitionPlatform/T03_CompetitionPlatform/Controllers/HomeController.cs
+++ T03_CompetitionPlatform/T03_CompetitionPlatform/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using T03_CompetitionPlatform.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace T03_CompetitionPlatform.Controllers
 {
@@ -27,6 +28,21 @@ namespace T03_CompetitionPlatform.Controllers
         {
             return View();
         }
+
+        // Activates redirect to login button, to login.cshtml
+        [HttpPost]
+        public ActionResult redirectLogin(IFormCollection formData)
+        {
+            return RedirectToAction("Login");
+        }
+
+        // Activates redirect to register button, to register.cshtml
+        [HttpPost]
+        public ActionResult redirectRegister(IFormCollection formData)
+        {
+            return RedirectToAction("Register");
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
