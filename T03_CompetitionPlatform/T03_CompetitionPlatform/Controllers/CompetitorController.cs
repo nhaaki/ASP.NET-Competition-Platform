0c529663bd444753335e08e3bf6d0874d9638e7c
Chng Boon Chong <S10208460@connect.np.edu.sg>
2021-07-04 21:22:47 +0800

    Added empty Competitor Controller & Competitor View Index

---
+++ T03_CompetitionPlatform/T03_CompetitionPlatform/Controllers/CompetitorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace T03_CompetitionPlatform.Controllers
+{
+    public class CompetitorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
