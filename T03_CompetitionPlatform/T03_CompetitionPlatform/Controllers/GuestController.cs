096f8b91d45da782dbb30e3fd1de05d46f88be1e
nhaaki <S10206177@connect.np.edu.sg>
2021-07-05 11:55:44 +0800

    Display competitions in Guest View and add navbar

--- T03_CompetitionPlatform/T03_CompetitionPlatform/Controllers/GuestController.cs
+++ T03_CompetitionPlatform/T03_CompetitionPlatform/Controllers/GuestController.cs
@@ -3,14 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using T03_CompetitionPlatform.DAL;
+using T03_CompetitionPlatform.Models;
 
 namespace T03_CompetitionPlatform.Controllers
 {
     public class GuestController : Controller
     {
+
+        private CompetitionDAL competitionContext = new CompetitionDAL();
+
         public ActionResult Index()
         {
-            return View();
+
+            List<Competition> compList = competitionContext.GetAllCompetitions();
+            return View(compList);
+            
         }
     }
 }
