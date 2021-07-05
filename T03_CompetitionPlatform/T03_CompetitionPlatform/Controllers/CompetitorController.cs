7303d68b66a91bba356a8e780741c11a76717b33
Chng Boon Chong <S10208460@connect.np.edu.sg>
2021-07-06 00:30:05 +0800

    Attempted Competitor Index (Not working)

--- T03_CompetitionPlatform/T03_CompetitionPlatform/Controllers/CompetitorController.cs
+++ T03_CompetitionPlatform/T03_CompetitionPlatform/Controllers/CompetitorController.cs
@@ -3,14 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using T03_CompetitionPlatform.DAL;
+using T03_CompetitionPlatform.Models;
 
 namespace T03_CompetitionPlatform.Controllers
 {
     public class CompetitorController : Controller
     {
+        private CompetitionDAL competitionContext = new CompetitionDAL();
         public IActionResult Index()
         {
-            return View();
+            List<Competition> competitionList = competitionContext.GetAllCompetitions();
+            return View(competitionList);
         }
     }
 }
--- T03_CompetitionPlatform/T03_CompetitionPlatform/Models/CompetitionSubmission.cs
+++ T03_CompetitionPlatform/T03_CompetitionPlatform/Models/CompetitionSubmission.cs
@@ -8,7 +8,25 @@ namespace T03_CompetitionPlatform.Models
 {
     public class CompetitionSubmission
     {
+        [Required]
         public int CompetitionID { get; set; }
+        
+        [Required]
         public int CompetitorID { get; set; }
+
+        [StringLength(255)]
+        public string FileSubmitted { get; set; }
+
+        [DisplayFormat(DataFormatString ="{yyyy-MM-dd }")]
+        public DateTime DateTimeFileUpload { get; set; } 
+
+        [StringLength(255)]
+        public string Appeal { get; set; }
+
+        [Required]
+        public int VoteCount { get; set; }
+
+        //[Range(1)]
+        public int Ranking { get; set; }
     }
 }
